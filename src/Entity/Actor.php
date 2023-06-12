18eb8e9cdce8bbd9a4b029fc28c8aa08a791e2d7
Ferdi <ferdi.sahin@etudiant.univ-reims.fr>
2023-06-12 14:16:04 +0200

    Ajout des propriétés, des accesseurs et des mutateurs de la classe Actor

--- src/Entity/Actor.php
+++ src/Entity/Actor.php
@@ -4,7 +4,132 @@ declare(strict_types=1);
 
 namespace Entity;
 
+use Database\MyPdo;
+use PDO;
+
 class Actor
 {
+    private ?int $id;
+    private int $avatarId;
+    private string $birthday;
+    private ?string $deathday;
+    private string $name;
+    private string $biography;
+    private string $placeOfBirth;
 
+    private function __construct()
+    {
+    }
+
+    /**
+     * @return int|null
+     */
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return int
+     */
+    public function getAvatarId(): int
+    {
+        return $this->avatarId;
+    }
+
+    /**
+     * @return string
+     */
+    public function getBirthday(): string
+    {
+        return $this->birthday;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getDeathday(): ?string
+    {
+        return $this->deathday;
+    }
+
+    /**
+     * @return string
+     */
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    /**
+     * @return string
+     */
+    public function getBiography(): string
+    {
+        return $this->biography;
+    }
+
+    /**
+     * @return string
+     */
+    public function getPlaceOfBirth(): string
+    {
+        return $this->placeOfBirth;
+    }
+
+    /**
+     * @param int|null $id
+     */
+    public function setId(?int $id): void
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @param int $avatarId
+     */
+    public function setAvatarId(int $avatarId): void
+    {
+        $this->avatarId = $avatarId;
+    }
+
+    /**
+     * @param string $birthday
+     */
+    public function setBirthday(string $birthday): void
+    {
+        $this->birthday = $birthday;
+    }
+
+    /**
+     * @param string|null $deathday
+     */
+    public function setDeathday(?string $deathday): void
+    {
+        $this->deathday = $deathday;
+    }
+
+    /**
+     * @param string $name
+     */
+    public function setName(string $name): void
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * @param string $biography
+     */
+    public function setBiography(string $biography): void
+    {
+        $this->biography = $biography;
+    }
+
+    /**
+     * @param string $placeOfBirth
+     */
+    public function setPlaceOfBirth(string $placeOfBirth): void
+    {
+        $this->placeOfBirth = $placeOfBirth;
+    }
 }
