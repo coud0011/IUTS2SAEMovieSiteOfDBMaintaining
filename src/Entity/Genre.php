5239bd9e40274c5c263b131164249c9091b52a49
Ferdi <ferdi.sahin@etudiant.univ-reims.fr>
2023-06-12 15:20:06 +0200

    Création de la méthode findById() dans la classe Genre

--- src/Entity/Genre.php
+++ src/Entity/Genre.php
@@ -27,4 +27,22 @@ class Genre
     {
         return $this->name;
     }
+
+    public static function findById(int $id): Genre
+    {
+        $stmt = MyPDO::getInstance()->prepare(
+            <<<'SQL'
+            SELECT *
+            FROM Image
+            WHERE id=:genreId
+        SQL
+        );
+        $stmt->bindParam(':genreId', $id, PDO::PARAM_INT);
+        $stmt->execute();
+        $genre=$stmt->fetchObject(Image::class);
+        if (!$genre) {
+            throw new EntityNotFoundException("findById() - Genre not found");
+        }
+        return $genre;
+    }
 }
