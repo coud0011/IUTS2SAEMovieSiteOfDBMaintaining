7164699e3d9ce2e77df1d8831096fcc5b60ccdbd
Ferdi <ferdi.sahin@etudiant.univ-reims.fr>
2023-06-12 14:57:18 +0200

    Ajout des propriétés et des accesseurs de la classe Image

--- src/Entity/Image.php
+++ src/Entity/Image.php
@@ -6,5 +6,22 @@ namespace Entity;
 
 class Image
 {
+    private int $id;
+    private string $jpeg;
 
+    /**
+     * @return int
+     */
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getJpeg(): string
+    {
+        return $this->jpeg;
+    }
 }
