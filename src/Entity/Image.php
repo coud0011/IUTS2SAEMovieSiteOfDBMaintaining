bee7d89d4a6bf951d855950227fe146ae046f3fa
Ferdi <ferdi.sahin@etudiant.univ-reims.fr>
2023-06-12 14:59:56 +0200

    Cr&tion de la méthode findById dans la classe Image

--- src/Entity/Image.php
+++ src/Entity/Image.php
@@ -4,6 +4,10 @@ declare(strict_types=1);
 
 namespace Entity;
 
+use Database\MyPdo;
+use Exception\EntityNotFoundException;
+use PDO;
+
 class Image
 {
     private int $id;
@@ -23,5 +27,23 @@ class Image
     public function getJpeg(): string
     {
         return $this->jpeg;
+    }
+
+    public static function findById(int $id): Image
+    {
+        $stmt = MyPDO::getInstance()->prepare(
+            <<<'SQL'
+            SELECT *
+            FROM Image
+            WHERE id=:imageId
+        SQL
+        );
+        $stmt->bindParam(':imageId', $id, PDO::PARAM_INT);
+        $stmt->execute();
+        $image=$stmt->fetchObject(Image::class);
+        if (!$image) {
+            throw new EntityNotFoundException("findById() - Image not found");
+        }
+        return $image;
     }
 }
