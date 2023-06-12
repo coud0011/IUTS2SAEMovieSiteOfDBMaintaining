8d670709b6e0ac6c6305722caefbe806d348ad70
Ferdi <ferdi.sahin@etudiant.univ-reims.fr>
2023-06-12 15:34:58 +0200

    Création de la méthode findByGenreId() dans la classe MoviesCollection

--- src/Entity/Collections/MoviesCollection.php
+++ src/Entity/Collections/MoviesCollection.php
@@ -11,12 +11,11 @@ use PDO;
 class MoviesCollection
 {
     /**
-
-    Méthode d'instance findAll
-    Permet de récupérer dans un array
-    tous les films de la class Movies
-    de notre base de donnée
-    @return Movie[]
+     * Méthode d'instance findAll
+     * Permet de récupérer dans un array
+     * tous les films de la class Movies
+     * de notre base de donnée
+     * @return Movie[]
      */
     public static function findAll(): array
     {
@@ -30,5 +29,27 @@ class MoviesCollection
         $stmt->execute();
         $stmt->setFetchMode(PDO::FETCH_CLASS, Movie::class);
         return $stmt->fetchAll();
+    }
+
+    /**
+     * Retourne tous les films appartenant au genre passé en paramètre
+     * @param int $genreId
+     * @return Movie[]
+     */
+    public function findByGenreId(int $genreId): array
+    {
+        $stmt = MyPDO::getInstance()->prepare(
+            <<<'SQL'
+            SELECT *
+            FROM Movie
+            WHERE id IN (SELECT movieId
+                         FROM movie_genre
+                         WHERE genreId=:genreId)
+            ORDER BY title
+        SQL
+        );
+        $stmt->bindParam(':genreId', $genreId, PDO::PARAM_INT);
+        $stmt->execute();
+        return $stmt->fetchAll(PDO::FETCH_CLASS, Movie::class);
     }
 }
