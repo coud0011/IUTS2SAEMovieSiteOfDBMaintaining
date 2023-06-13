de2176e177844cc84fd810521b3dabb6a223f3dd
Ferdi SAHIN <ferdi.sahin@etudiant.univ-reims.fr>
2023-06-13 18:37:40 +0200

    Suppression des tests inutiles dans actor-save

--- public/admin/actor-save.php
+++ public/admin/actor-save.php
@@ -5,18 +5,6 @@ use Exception\ParameterException;
 use Html\Form\ActorForm;
 
 try {
-    if (!isset($_POST['birthday'])) {
-        throw new ParameterException('Parameter birthday not found');
-    }
-    if (!isset($_POST['name'])) {
-        throw new ParameterException('Parameter name not found');
-    }
-    if (!isset($_POST['biography'])) {
-        throw new ParameterException('Parameter biography not found');
-    }
-    if (!isset($_POST['placeOfBirth'])) {
-        throw new ParameterException('Parameter placeOfBirth not found');
-    }
     $artistForm=new ActorForm();
     $artistForm->setEntityFromQueryString();
     $artistForm->getActor()->save();
