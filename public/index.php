5cfc2f3cec6eae4463b2abc9da17a5573aaad1eb
Axel COUDROT <axel.coudrot@free.fr>
2023-06-13 13:07:06 +0200

    Filtrage par genre mis en place

--- public/index.php
+++ public/index.php
@@ -2,6 +2,7 @@
 
 declare(strict_types=1);
 
+use Entity\Collections\GenresCollection;
 use Entity\Collections\MoviesCollection;
 use Entity\Image;
 use Entity\Movie;
@@ -10,8 +11,30 @@ use Html\WebPage;
 
 
 $webpage= new WebPage("Films");
-$moviesCollection=MoviesCollection::findAll();
+if(!isset($_GET["genreId"]) || !ctype_digit($_GET["genreId"])){
+    $moviesCollection=MoviesCollection::findAll();
+}
+else{
+    $moviesCollection=MoviesCollection::findByGenreId((int)$_GET["genreId"]);
+}
 $webpage->appendCssUrl("css/style.css");
+$webpage->appendContent('<form method="GET" action="index.php" class="genres"><h1>Filtrage par genre : </h1><select name="genreId">');
+$genres=GenresCollection::findAll();
+foreach($genres as $genre){
+    $webpage->appendContent(<<<HTML
+<option class="genre" value="{$genre->getId()}"
+HTML
+    );
+    if(isset($_GET["genreId"]) && $_GET["genreId"]==="{$genre->getId()}"){
+        $webpage->appendContent(" selected");
+    }
+    $webpage->appendContent(">{$genre->getName()}</option>");
+}
+$webpage->appendContent('<a href="index.php"><option value="" class="genre"');
+if(!isset($_GET["genreId"]) || !ctype_digit($_GET["genreId"])){
+    $webpage->appendContent(" selected");
+}
+$webpage->appendContent('>All</option></a></select><input value="Choisir ce genre" type="submit"></form>');
 $webpage->appendContent('<main>');
 
 $it=0;
